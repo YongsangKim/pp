277ffdf54488649846248cb4d7c8d46ff78dfb32
kys <sesses66@hanmail.net>
2022-04-21 17:28:22 +0900

    가이드 작성중

---
+++ js/guide.js
@@ -0,0 +1,40 @@
+const header = document.getElementsByTagName('header')[0];
+header.innerHTML = '<h1><a>U<sup>+</sup>비즈마켓 파트너포털 퍼블리싱 가이드</a></h1>' +
+'<a href="../../모바일_비즈마켓_프론트_화면목록.html" target="_blank">퍼블리싱화면목록</a>';
+
+const nav = document.getElementsByTagName('nav')[0];
+nav.innerHTML = '<ul>' +
+'				<li><a href="index.html">기본정책</a></li>' +
+'				<li><a href="button.html">UI</a>' +
+'					<ul>' +
+'						<li><a href="button.html">Button</a></li>' +
+'						<li><a href="form.html">Form</a></li>' +
+'						<li><a href="form_group.html">Form Group</a></li>' +
+'						<li><a href="list.html">Title / List</a></li>' +
+'						<li><a href="table.html">Tables</a></li>' +
+'						<li><a href="tab.html">Tab</a></li>' +
+'						<li><a href="accordion.html">Accordion</a></li>' +
+'						<li><a href="dropdown.html">Dropdown</a></li>' +
+'						<li><a href="popup.html">Popup</a></li>' +
+'					</ul>' +
+'				</li>' +
+'				<li><a href="layout.html">레이아웃</a>' +
+'                   <ul>' +
+'						<li><a href="layout.html">header, footer</a></li>' +
+'						<li><a href="main_banner.html">메인배너</a></li>' +
+'					</ul>' +
+'               </li>' +
+'				<li><a href="../template/서비스카드.html">메인 컨텐츠등록</a>' +
+'					<ul>' +
+'						<li><a href="../template/서비스카드.html">서비스카드</a></li>' +
+'						<li><a href="../template/서비스특징.html">서비스특징</a></li>' +
+'					</ul>' +
+'				</li>' +
+'				<li><a href="../template/서비스별_요금안내.html">서비스별 컨텐츠등록</a>' +
+'					<ul>' +
+'						<li><a href="../template/서비스별_요금안내.html">요금안내</a></li>' +
+'						<li><a href="../template/서비스별_서비스특징.html">서비스특징</a></li>' +
+'						<li><a href="../template/서비스별_주요기능.html">주요기능</a></li>' +
+'					</ul>' +
+'				</li>' +
+'			</ul>';
